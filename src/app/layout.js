08c48326d527e2520c8b4f8bb76a4e79d71551f8
unknown <mohdsshadab552004@gmail.com>
2025-04-16 03:05:34 +0530

    final Commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,14 @@
-import { Geist} from "next/font/google";
+import { Geist,Geist_Mono} from "next/font/google";
 import "./globals.css";
 import { CategoryProvider } from "@/context/CategoryContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+});
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export default function RootLayout({ children }) {
